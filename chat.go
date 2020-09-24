2d50eefa1af6a4f390d3ceb9e3ac8d926b98c55a
Blink The Things <blinkthethings@gmail.com>
2020-09-24 00:57:08 -0400

    Add speaking and address command line args

--- chat.go
+++ chat.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Chat")
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: chat [listen addr] [speak addr]\n")
+		os.Exit(0)
+	}
 
-	localAddr, err := net.ResolveUDPAddr("udp", "localhost:1234")
+	fmt.Println("Chat")
+	fmt.Println("----")
+
+	listenAddr := os.Args[1]
+	speakAddr := os.Args[2]
+
+	go listen(listenAddr)
+
+	speak(speakAddr)
+}
+
+func listen(listenAddr string) {
+	localAddr, err := net.ResolveUDPAddr("udp", listenAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,12 +39,34 @@ func main() {
 	dgram := make([]byte, 65535)
 
 	for {
-		cnt, remoteAddr, err := udpConn.ReadFromUDP(dgram)
+		cnt, _, err := udpConn.ReadFromUDP(dgram)
 		if err != nil {
 			log.Print(err)
 		}
 
-		fmt.Printf("Read %d bytes from %s\n", cnt, remoteAddr.String())
+		fmt.Printf("< %s", string(dgram[:cnt]))
+	}
+}
+
+func speak(speakAddr string) {
+	remoteAddr, err := net.ResolveUDPAddr("udp", speakAddr)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	udpConn, err := net.DialUDP("udp", nil, remoteAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, _ := reader.ReadString('\n')
+		fmt.Printf("> %s", line)
+
+		_, err = udpConn.Write([]byte(line))
+		if err != nil {
+			log.Print(err)
+		}
+	}
 }
